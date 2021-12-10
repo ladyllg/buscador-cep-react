2d39fbc20cd50915a44a5e2aa9536989e1bb3d1c
LAURA LIMA <lauralimaufam@gmail.com>
2021-12-10 10:57:34 -0400

    setting functionality

--- src/Content.js
+++ src/Content.js
@@ -1,23 +1,46 @@
 import { BiSearchAlt } from "react-icons/bi";
 import './content.css'
+import { useState } from 'react'
+import api from './services/api';
 
 function Content() {
+
+    const [input, setInput] = useState('')
+    const [cep, setCep] = useState({})
+
+    async function handleSearch(){
+        if(input !== ''){
+            try{
+                const response = await api.get(`${input}/json`);
+                setCep(response.data);
+                setInput('');
+            }catch{
+                console.log('zip cop not found')
+                setInput('')
+            }
+        }
+    }
+
     return (
         <div className="container">
             <h1 className="title">Find Adress</h1>
             <div className="form-group">
-                <input type="text" className="input-text" placeholder="Search Zip Code..."/>
-                <button className="btn-search">
+                <input type="text" className="input-text" placeholder="Search Zip Code..."
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}/>
+                <button className="btn-search" onClick={handleSearch}>
                     <BiSearchAlt size={25} color="rgb(55, 55, 55)"/>
                 </button>
             </div>
-            <main className="main">
-                <h2>ZipCode: 2312334</h2>
-                <span>Rua qualquer</span>
-                <span>Complemento</span>
-                <span>Bairro</span>
-                <span>Cidade</span>
-            </main>
+            {Object.keys(cep).length > 0 && (
+                <main className="main">
+                    <h2>ZipCode: { cep.cep }</h2>
+                    <span>{ cep.logradouro }</span>
+                    <span>Complemento: { cep.complemento }</span>
+                    <span>{ cep.bairro }</span>
+                    <span>{ cep.localidade} - { cep.uf }</span>
+                </main>
+            )}                
         </div>
     )
 }
